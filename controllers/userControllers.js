64d467f216669d3c531a1f09459969f0ca741db3
jk2006k <jk05kishoore@gmail.com>
2025-02-03 12:03:19 +0530

    express-signup

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,23 +1,26 @@
-const User  = require('../models/userModels')
+const User = require('../models/userModels')
 
 const signUp = async (req, res) =>{
     try {
         console.log(req.body)
+        if (!req.body) {
+            return res.status(400).json({success: false, msg: "Request body is missing"})
+        }
         const {username, email, password, dateOfBirth} = req.body
         if (username.trim() == ""){
-            res.status(400).json({success: false, msg: "Username Cannot be Empty"})
+            return res.status(400).json({success: false, msg: "Username Cannot be Empty"})
         }
         if (email.trim() == ""){
-            res.status(400).json({success: false, msg: "Username Cannot be Empty"})
+            return res.status(400).json({success: false, msg: "Email Cannot be Empty"})
         }
         if (password.length < 8 || password.length > 16){
-            res.status(400).json({success: false, msg: "Password length should be greater than 8 or less than or equal to 16"})
+            return res.status(400).json({success: false, msg: "Password length should be greater than 8 or less than or equal to 16"})
         }
-        const newUser = User({username, email, password, dateOfBirth})
+        const newUser = new User({username, email, password, dateOfBirth})
         await newUser.save()
-        res.status(201).json({success: true, msg: "User Created Successfully"})
+        return res.status(201).json({success: true, msg: "User Created Successfully"})
     } catch (error) {
-        res.status(500).json({success: false, msg: "Internal Server Error", desc: error.message})
+        return res.status(500).json({success: false, msg: "Internal Server Error", desc: error.message})
     }
 }
 
